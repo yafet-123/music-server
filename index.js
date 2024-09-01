8b8c5433aa0081c72f6c02a33c8482d96b6a6a5c
Yafet Addisu <76257965+yafet-123@users.noreply.github.com>
2024-09-01 00:41:15 +0000

    edite the index.js and docker file

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const songRoutes = require('./routes/songs.js'); 
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/songs', songRoutes);
